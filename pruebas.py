024edbf82df832556d87c242bc1ce95763641161
larj3852 <larj385269@gmail.com>
2020-09-21 19:59:13 -0500

    Conflictos

--- pruebas.py
+++ pruebas.py
@@ -1,10 +1,14 @@
 """
 Este archivo nada mas es para hacer pruebas uniarias joven
 """
-
 import LibraryTT.Lidar3D as Lidar
+from time import sleep
 
 scan = Lidar.Scaner3D()
-a = scan.Scanear(Angulo_Init=130,Angulo_Fin=30,paso=-2,plotear=True)
-del(scan.lidar)
-del(scan)
+a = scan.Scanear(Angulo_Init=110,Angulo_Fin=60,paso=-2,plotear=True)
+sleep(2)
+# a = scan.Scanear(Angulo_Init=130,Angulo_Fin=30,paso=-2,plotear=True)
+# sleep(2)
+# a = scan.Scanear(Angulo_Init=130,Angulo_Fin=30,paso=-1,plotear=True)
+# del(scan.lidar)
+# del(scan)
